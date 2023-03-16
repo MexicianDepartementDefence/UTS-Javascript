9f25748500ef54afd1ebd5b309f1aeaeb5c5bda8
MexicianDepartementDefence <faisal216093@gmail.com>
2023-03-16 10:58:27 +0700

    first commit

---
+++ utsjs.js
@@ -0,0 +1,78 @@
+/*
+//soal nomor 1
+class jenishewan {
+    constructor(hewan ,kaki, pemakan, jenis){
+this.hewan = hewan;
+this.kaki = kaki;
+this.pemakan = pemakan;
+this.jenis = jenis;
+
+}
+mamalia(){    console.log (`Hewan ${this.hewan} Mempunyai ${this.kaki} Kaki Dan Ia Pemakan ${this.pemakan} Dan Ia Berjenis ${this.jenis}`)
+}
+
+
+amfibi(){
+    console.log (`Hewan ${this.hewan} Mempunyai ${this.kaki} Kaki Dan Ia Pemakan ${this.pemakan} Dan Ia Berjenis ${this.jenis}`)
+}
+burung(){
+    console.log (`Hewan ${this.hewan} Mempunyai ${this.kaki} Kaki Dan Ia Pemakan ${this.pemakan} Dan Ia Berjenis ${this.jenis}`)
+
+}
+reptil(){
+    console.log (`Hewan ${this.hewan} Mempunyai ${this.kaki} Kaki Dan Ia Pemakan ${this.pemakan} Dan Ia Berjenis ${this.jenis}`)
+
+}
+
+
+
+}
+
+let ayam = new jenishewan("ayam", 2, "omnivora", "burung");
+let kucing = new jenishewan("Kucing", 4, "Karnivora", "Mamalia");
+let katak = new jenishewan("Katak", 2, "insektivora", "amfibi");
+let monyet = new jenishewan("Monyet", 2, "Omnivora", "Mamalia");
+
+console.table(katak)
+katak.amfibi()
+console.table(ayam)
+ayam.burung()
+console.table(monyet)
+monyet.mamalia()
+console.table(kucing)
+kucing.mamalia()*/
+
+// soal nomor 2
+
+/*let klik = document.getElementById("Klik");
+let mouse = document.getElementById("Mouse");
+let remove = document.getElementById("Remove");
+
+klik.addEventListener("click", function () {
+    document.getElementById("demo").innerHTML = "Berhasil";
+});
+
+klik.addEventListener("dblclick", function(){
+    alert("Tombol Di kilk 2 Kali")
+});
+
+mouse.addEventListener("mouseenter", function(){
+    
+   const h1 = document.createElement("h1");
+    const textNode = document.createTextNode("X RPL");
+    h1.appendChild(textNode);
+    document.body.appendChild(h1);
+    h1.style.color = "red";
+})
+
+mouse.addEventListener("mouseout", function(){
+    const h1 = document.createElement("h1");
+    const textNode = document.createTextNode("X RPL");
+    h1.appendChild(textNode);
+    document.body.appendChild(h1);
+    h1.style.color = "green";
+})
+
+remove.addEventListener("click", function(){
+    document.write();
+})*/
